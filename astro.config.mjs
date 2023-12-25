6fbc90ba912fa6476b03a3488dc69acbe0e5302c
Cas74l4n <catelan.15@outlook.es>
2023-12-25 17:59:55 -0600

    finish3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://github.com/Cas74l4n.github.io',
+	base: '/Cas74l4n',
 	integrations: [mdx(), sitemap()],
 });
